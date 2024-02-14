35ae37a225b86f2c1189fb0b203995b045d26c46
Orulirn <ewencarre@free.fr>
2024-02-14 16:44:31 +0100

    ajouts de différentes activité / class

---
+++ app/src/main/java/Model/Notes.java
@@ -0,0 +1,8 @@
+package Model;
+
+
+public class Notes {
+    private int id;
+    private String titre;
+    private String body;
+}
---
+++ app/src/main/java/com/example/mini_projet_r411/AddNotes.java
@@ -0,0 +1,14 @@
+package com.example.mini_projet_r411;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_notes);
+    }
+}
