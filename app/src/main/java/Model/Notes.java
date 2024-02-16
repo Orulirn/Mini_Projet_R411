570aa83423b2ffb80b8bc5799f7b643599e7be5e
Orulirn <ewencarre@free.fr>
2024-02-16 08:48:12 +0100

    base de la classe Notes.java

--- app/src/main/java/Model/Notes.java
+++ app/src/main/java/Model/Notes.java
@@ -1,9 +1,46 @@
 package Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity(name = "Notes")
 public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "titre")
     private String titre;
+    @Column(name = "body")
     private String body;
+
+    public Notes() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
