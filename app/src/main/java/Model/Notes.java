55fdc8d2152d28d243de140751967058bc041274
PierreBadelek <pierre.badelek@uphf.fr>
2024-02-19 14:15:15 +0100

    suppression hibernate

--- app/src/main/java/Model/Notes.java
+++ app/src/main/java/Model/Notes.java
@@ -1,20 +1,12 @@
 package Model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity(name = "Notes")
+
 public class Notes {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     private int id;
-    @Column(name = "titre")
+
     private String titre;
-    @Column(name = "body")
     private String body;
 
     public Notes() {
