da22ed04fc18fa730fdfaf548700ea88299821e2
Orulirn <ewencarre@free.fr>
2024-02-19 14:12:53 +0100

    création des premiers intents

--- app/src/main/java/com/example/mini_projet_r411/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_r411/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mini_projet_r411;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void ChangeActivityAddNote(){
+        Intent intent = new Intent(this, AddNotes.class);
+
+        startActivity(intent);
+    }
+
+    private void ChangeActivityModifyNotes(){
+        //TODO ajouter la fonctionnalité de modification de note
     }
 }
